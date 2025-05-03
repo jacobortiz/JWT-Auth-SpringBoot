a0b07bcb0f1129b1e2a50802cd5778ec7914b8f6
Jacob Ortiz <jacobortiz90@gmail.com>
2025-05-02 19:44:21 -0700

    Extend User entity with authentication details

--- src/main/java/com/jacobortiz/auth_api/entities/User.java
+++ src/main/java/com/jacobortiz/auth_api/entities/User.java
@@ -3,12 +3,16 @@ package com.jacobortiz.auth_api.entities;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Table(name = "users")
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
@@ -31,7 +35,39 @@ public class User {
     @Column(name = "updated_at")
     private Date updatedAt;
 
-//    Getters and Setters
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     public Integer getId() {
         return id;
@@ -55,10 +91,6 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
